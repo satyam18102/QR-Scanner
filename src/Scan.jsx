6cb0e61feb6ec31e57690dd0a1149c265e034ebf
Satyam Sinha <satyamsinha340@gmail.com>
2025-01-29 16:43:24 +0530

    Updating Scanner

--- src/Scan.jsx
+++ src/Scan.jsx
@@ -3,9 +3,6 @@ import { useEffect, useState } from 'react';
 import './Scan.css';
 export default function Scan(){
     const [scanResult,setScanResult]=useState(null);
-    const [name,setName]=useState(null);
-    let arr=["abcd",];
-    let arr1=["ab",];
     useEffect(()=>{
         const scanner=new Html5QrcodeScanner('reader',{
             qrbox:{
@@ -25,30 +22,13 @@ export default function Scan(){
             // console.log(err)
         }
     },[])
-    function handleChange(){
-        console.log(scanResult)
-    }
-    let handleChange1=(evt)=>{
-        setName(evt.target.value);
-    }
-    let handleSubmit=(evt)=>{
-        evt.preventDefault();
-        arr.push(scanResult);
-        arr1.push(name);
-        console.log(arr);
-        console.log(arr1);
-    }
     return(
         <>
         <div className="App">
         <h1>Scanner</h1>
         {scanResult
         ?<div className='center'>
-            <form onSubmit={handleSubmit} className='center'>
-                <input onChange={handleChange} value={scanResult} className='center'></input>
-                <input onChange={handleChange1} className='center'></input>
-                <button type='Submit'>Sumbit</button>
-            </form>
+            <h1>{scanResult}</h1>
         </div>
         :<div id='reader'></div>
     }
