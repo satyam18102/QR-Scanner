a26844910bdee1d97ff8f037cebdc5396f275e5e
Satyam Sinha <satyamsinha340@gmail.com>
2025-01-30 12:10:37 +0530

    Adding More Functions

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Scanner from './Scanner.jsx'
+import Scan from './Scan.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Scanner />
+    <Scan />
   </StrictMode>,
 )
